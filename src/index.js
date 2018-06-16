f1b7909596e5b6855300cc076d42a8312d7aa1d7
pfreema1 <pfreema1@gmail.com>
2018-06-16 11:58:02 -0400

    BEGIN

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@
 
 (function() {
   class Player {
-    constructor() {
-      this.name = 'foooooobear';
+    constructor(name) {
+      this.name = name;
+      this.width = 20;
+      this.height = 20;
     }
 
     sayName() {
@@ -11,7 +13,17 @@
     }
   }
 
-  let player = new Player();
+  // set up canvas
+  const c = document.getElementById('myCanvas');
+  const ctx = c.getContext('2d');
+  c.width = window.innerWidth;
+  c.Height = window.innerHeight;
+
+  ctx.moveTo(0, 0);
+  ctx.lineTo(200, 100);
+  ctx.stroke();
+
+  let player = new Player('poophead');
 
   player.sayName();
 })();
