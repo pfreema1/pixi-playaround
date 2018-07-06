df7de28d1e603b990d5852e33b0650b4ab30f468
Paul <pfreema1@gmail.com>
2018-07-06 18:38:06 -0400

    moo

--- src/index.js
+++ src/index.js
@@ -10,23 +10,38 @@ const app = new PIXI.Application(window.innerWidth, window.innerHeight, {
 
 document.body.appendChild(app.view);
 
+const returnArrOfMovers = () => {
+  let arrayOfMovers = [];
+
+
+
+  return arrayOfMovers;
+}
+
 
 
 attractorCircle.moverCircleRef = moverCircle;
 
+const blurFilter = new PIXI.filters.BlurFilter(8, 3);
+const noiseFilter = new PIXI.filters.NoiseFilter();
+app.stage.filters = [blurFilter, noiseFilter];
+let count = 0;
+
+
 app.stage.addChild(attractorCircle, moverCircle);
-
-
-
-
-
-
-
 
 // animate
 app.ticker.add(function(delta) {
   let force = attractorCircle.attract();
   moverCircle.applyForce(force, delta);
 
+
+  let matrix = blurFilter.matrix;
+
+  count += 0.1;
+
+  blurFilter.blur = force.x * 5;
+
+  
 });
 
